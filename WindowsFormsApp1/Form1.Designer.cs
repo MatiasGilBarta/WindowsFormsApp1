59f8ae738757c4fabdcbb20a5947aee9cc20c708
MatiasGilBarta <matiasgilbarta.dev@gmail.com>
2025-03-14 16:56:27 -0300

    Se agrega el Proyecto

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresarRepuesto = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDesahbilitarRepuesto = new System.Windows.Forms.Button();
+            this.btnModificarRepuesto = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnIngresarRepuesto
+            // 
+            this.btnIngresarRepuesto.BackColor = System.Drawing.Color.White;
+            this.btnIngresarRepuesto.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnIngresarRepuesto.FlatAppearance.BorderSize = 10;
+            this.btnIngresarRepuesto.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnIngresarRepuesto.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnIngresarRepuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresarRepuesto.Location = new System.Drawing.Point(504, 44);
+            this.btnIngresarRepuesto.Name = "btnIngresarRepuesto";
+            this.btnIngresarRepuesto.Size = new System.Drawing.Size(170, 60);
+            this.btnIngresarRepuesto.TabIndex = 0;
+            this.btnIngresarRepuesto.Text = "Ingresar Repuesto";
+            this.btnIngresarRepuesto.UseVisualStyleBackColor = false;
+            this.btnIngresarRepuesto.Click += new System.EventHandler(this.btnIngresarRepuesto_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(353, 233);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnDesahbilitarRepuesto
+            // 
+            this.btnDesahbilitarRepuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesahbilitarRepuesto.Location = new System.Drawing.Point(504, 119);
+            this.btnDesahbilitarRepuesto.Name = "btnDesahbilitarRepuesto";
+            this.btnDesahbilitarRepuesto.Size = new System.Drawing.Size(170, 60);
+            this.btnDesahbilitarRepuesto.TabIndex = 2;
+            this.btnDesahbilitarRepuesto.Text = "Deshabilitar Repuesto";
+            this.btnDesahbilitarRepuesto.UseVisualStyleBackColor = true;
+            this.btnDesahbilitarRepuesto.Click += new System.EventHandler(this.btnDesahbilitarRepuesto_Click);
+            // 
+            // btnModificarRepuesto
+            // 
+            this.btnModificarRepuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificarRepuesto.Location = new System.Drawing.Point(504, 195);
+            this.btnModificarRepuesto.Name = "btnModificarRepuesto";
+            this.btnModificarRepuesto.Size = new System.Drawing.Size(170, 60);
+            this.btnModificarRepuesto.TabIndex = 3;
+            this.btnModificarRepuesto.Text = "Modificar Repuesto";
+            this.btnModificarRepuesto.UseVisualStyleBackColor = true;
+            this.btnModificarRepuesto.Click += new System.EventHandler(this.btnModificarRepuesto_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(504, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 60);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnModificarRepuesto);
+            this.Controls.Add(this.btnDesahbilitarRepuesto);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnIngresarRepuesto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresarRepuesto;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDesahbilitarRepuesto;
+        private System.Windows.Forms.Button btnModificarRepuesto;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        ListarStock _e;
+        public Form1()
+        {
+            InitializeComponent();
+            _e = new ListarStock();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnIngresarRepuesto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Stock nuevoStock = new Stock(
+                    0,  // Id_stock no se envía porque es autonumerado
+                    int.Parse(Interaction.InputBox("Nro de repuesto: ")),
+                    Interaction.InputBox("Nombre del repuesto: "),
+                    Interaction.InputBox("Descripcion: "),
+                    int.Parse(Interaction.InputBox("Cantidad: "))
+                );
+
+                _e.AgregarStock(nuevoStock);
+                Mostrar(dataGridView1, _e.RetornarStock());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void Mostrar(DataGridView pDGV, Object pObject)
+        {
+            pDGV.DataSource = null;
+            pDGV.DataSource = pObject;
+        }
+
+        private void btnDesahbilitarRepuesto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    int idStock = ((Stock)dataGridView1.SelectedRows[0].DataBoundItem).Id_stock;
+                    _e.BorrarStock(idStock);
+                    Mostrar(dataGridView1, _e.RetornarStock());
+                }
+                else
+                {
+                    MessageBox.Show("No hay registros para borrar.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnModificarRepuesto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    Stock stock = (Stock)dataGridView1.SelectedRows[0].DataBoundItem;
+                    stock.Nombre_repuesto = Interaction.InputBox("Ingrese nombre del repuesto", "", stock.Nombre_repuesto);
+                    stock.Descripcion = Interaction.InputBox("Ingrese descripcion del repuesto", "", stock.Descripcion);
+                    stock.Cantidad = int.Parse(Interaction.InputBox("Ingrese la cantidad", "", stock.Cantidad.ToString()));
+
+                    _e.ModificarStock(stock);
+                    Mostrar(dataGridView1, _e.RetornarStock());
+                }
+                else
+                {
+                    MessageBox.Show("No hay registros para modificar.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public class ListarStock
+        {
+            private string connectionString;
+
+            public ListarStock()
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["ProyectoGestor"].ConnectionString;
+            }
+
+            // 🟢 Método para Agregar un Stock a la base de datos
+            public void AgregarStock(Stock pStock)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO Stock (Nro_repuesto, Nombre_repuesto, Descripcion, Cantidad) " +
+                                   "VALUES (@NroRepuesto, @Nombre, @Descripcion, @Cantidad)";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@NroRepuesto", pStock.Nro_repuesto);
+                    cmd.Parameters.AddWithValue("@Nombre", pStock.Nombre_repuesto);
+                    cmd.Parameters.AddWithValue("@Descripcion", pStock.Descripcion);
+                    cmd.Parameters.AddWithValue("@Cantidad", pStock.Cantidad);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            // 🔵 Método para Listar los Stocks desde la base de datos
+            public List<Stock> RetornarStock()
+            {
+                List<Stock> listaStock = new List<Stock>();
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT Id_stock, Nro_repuesto, Nombre_repuesto, Descripcion, Cantidad FROM Stock";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        listaStock.Add(new Stock(
+                            reader.GetInt32(0),  // Id_stock (Autonumerado)
+                            reader.GetInt32(1),  // Nro_repuesto
+                            reader.GetString(2), // Nombre_repuesto
+                            reader.GetString(3), // Descripcion
+                            reader.GetInt32(4)   // Cantidad
+                        ));
+                    }
+                }
+                return listaStock;
+            }
+
+            // 🟠 Método para Eliminar un Stock
+            public void BorrarStock(int idStock)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM Stock WHERE Id_stock = @IdStock";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@IdStock", idStock);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            // 🔴 Método para Modificar un Stock
+            public void ModificarStock(Stock pStock)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Stock SET Nombre_repuesto = @Nombre, Descripcion = @Descripcion, Cantidad = @Cantidad " +
+                                   "WHERE Id_stock = @IdStock";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@IdStock", pStock.Id_stock);
+                    cmd.Parameters.AddWithValue("@Nombre", pStock.Nombre_repuesto);
+                    cmd.Parameters.AddWithValue("@Descripcion", pStock.Descripcion);
+                    cmd.Parameters.AddWithValue("@Cantidad", pStock.Cantidad);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public class Stock
+        {
+            public int Id_stock { get; set; }  // Ahora tiene el ID autonumerado
+            public int Nro_repuesto { get; set; }
+            public string Nombre_repuesto { get; set; }
+            public string Descripcion { get; set; }
+            public int Cantidad { get; set; }
+
+            public Stock(int pId_stock, int pNro_repuesto, string pNombre_repuesto, string pDescripcion, int pCantidad)
+            {
+                Id_stock = pId_stock;
+                Nro_repuesto = pNro_repuesto;
+                Nombre_repuesto = pNombre_repuesto;
+                Descripcion = pDescripcion;
+                Cantidad = pCantidad;
+            }
+        }
+    }
+}
